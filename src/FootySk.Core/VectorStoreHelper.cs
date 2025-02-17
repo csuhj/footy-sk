03fcdd9da3d8704be5650e4bca44270657e724f9
Adam Robertson <adam.robertson@apteco.com>
2025-02-17 08:29:22 +0000

    Removed some slightly out of date VectorStoreHelper comments

--- src/FootySk.Core/VectorStoreHelper.cs
+++ src/FootySk.Core/VectorStoreHelper.cs
@@ -29,7 +29,6 @@ public class VectorStoreHelper
     public static async Task PopulatePlayersVectorStore(IVectorStore vectorStore, ITextEmbeddingGenerationService textEmbeddingService, string rootPath, Dictionary<string, string> positionAbbreviationToNameMap) {
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-        // Choose a collection from the database and specify the type of key and record stored in it via Generic parameters.
         var collection = GetPlayersCollection(vectorStore);
 
         if (await collection.CollectionExistsAsync()) {
@@ -37,7 +36,6 @@ public class VectorStoreHelper
             return;
         }
 
-        // Create the collection if it doesn't exist yet.
         await collection.CreateCollectionIfNotExistsAsync();
 
         PlayerRecord[] players = await new PlayerCsvParser(Path.Combine(rootPath, "src/FootySk.Database/all_players.csv")).Parse();
@@ -46,12 +44,8 @@ public class VectorStoreHelper
         Func<PlayerRecord, bool> playerFilter = p => p.League == "Premier League";
         Console.WriteLine($"Populating players collection from database with {players.Count(playerFilter)} players");
 
-        //See https://github.com/MicrosoftDocs/semantic-kernel-docs/blob/main/semantic-kernel/concepts/vector-store-connectors/vector-search.md
-        //    https://learn.microsoft.com/en-us/semantic-kernel/concepts/vector-store-connectors/vector-search?pivots=programming-language-csharp
-        //For now just update the first 10 players
         foreach (var player in players.Where(playerFilter))
         {
-            // Create a record and generate a vector for the description using your chosen embedding generation implementation.
             await collection.UpsertAsync(await Player.Create(player, textEmbeddingService, positionAbbreviationToNameMap));
         }
     }
@@ -60,7 +54,6 @@ public class VectorStoreHelper
     public static async Task PopulateAttributeDataVectorStore(IVectorStore vectorStore, ITextEmbeddingGenerationService textEmbeddingService, string rootPath) {
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-        // Choose a collection from the database and specify the type of key and record stored in it via Generic parameters.
         var collection = GetAttributeDetailsCollection(vectorStore);
 
         if (await collection.CollectionExistsAsync()) {
@@ -68,18 +61,13 @@ public class VectorStoreHelper
             return;
         }
 
-        // Create the collection if it doesn't exist yet.
         await collection.CreateCollectionIfNotExistsAsync();
 
         AttributeDetailsRecord[] attributeDetails = AttributeDetailsDataParser.GetAttributeDetails(Path.Combine(rootPath, "src/FootySk.Database/attribute_details_data.json"));
         Console.WriteLine($"Populating attribute details collection from database with {attributeDetails.Length} items");
 
-        //See https://github.com/MicrosoftDocs/semantic-kernel-docs/blob/main/semantic-kernel/concepts/vector-store-connectors/vector-search.md
-        //    https://learn.microsoft.com/en-us/semantic-kernel/concepts/vector-store-connectors/vector-search?pivots=programming-language-csharp
-        //For now just update the first 10 players
         foreach (var attributeDetail in attributeDetails)
         {
-            // Create a record and generate a vector for the description using your chosen embedding generation implementation.
             await collection.UpsertAsync(await AttributeDetails.Create(attributeDetail, textEmbeddingService));
         }
     }
@@ -88,7 +76,6 @@ public class VectorStoreHelper
     public static async Task PopulatePositionDataVectorStore(IVectorStore vectorStore, ITextEmbeddingGenerationService textEmbeddingService, string rootPath) {
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-        // Choose a collection from the database and specify the type of key and record stored in it via Generic parameters.
         var collection = GetPositionDetailsCollection(vectorStore);
 
         if (await collection.CollectionExistsAsync()) {
@@ -96,18 +83,13 @@ public class VectorStoreHelper
             return;
         }
 
-        // Create the collection if it doesn't exist yet.
         await collection.CreateCollectionIfNotExistsAsync();
 
         PositionDetailsRecord[] positionDetails = PositionDetailsDataParser.GetPositionDetails(Path.Combine(rootPath, "src/FootySk.Database/position_details_data.json"));
         Console.WriteLine($"Populating position details collection from database with {positionDetails.Length} items");
 
-        //See https://github.com/MicrosoftDocs/semantic-kernel-docs/blob/main/semantic-kernel/concepts/vector-store-connectors/vector-search.md
-        //    https://learn.microsoft.com/en-us/semantic-kernel/concepts/vector-store-connectors/vector-search?pivots=programming-language-csharp
-        //For now just update the first 10 players
         foreach (var positionDetail in positionDetails)
         {
-            // Create a record and generate a vector for the description using your chosen embedding generation implementation.
             await collection.UpsertAsync(await PositionDetails.Create(positionDetail, textEmbeddingService));
         }
     }
@@ -116,7 +98,6 @@ public class VectorStoreHelper
     public static async Task PopulatePlaystyleDataVectorStore(IVectorStore vectorStore, ITextEmbeddingGenerationService textEmbeddingService, string rootPath) {
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-        // Choose a collection from the database and specify the type of key and record stored in it via Generic parameters.
         var collection = GetPlaystyleDetailsCollection(vectorStore);
 
         if (await collection.CollectionExistsAsync()) {
@@ -124,18 +105,13 @@ public class VectorStoreHelper
             return;
         }
 
-        // Create the collection if it doesn't exist yet.
         await collection.CreateCollectionIfNotExistsAsync();
 
         PlaystyleDetailsRecord[] playstyleDetails = PlaystyleDetailsDataParser.GetPlaystyleDetails(Path.Combine(rootPath, "src/FootySk.Database/playstyle_details_data.json"));
         Console.WriteLine($"Populating playstyle details collection from database with {playstyleDetails.Length} items");
 
-        //See https://github.com/MicrosoftDocs/semantic-kernel-docs/blob/main/semantic-kernel/concepts/vector-store-connectors/vector-search.md
-        //    https://learn.microsoft.com/en-us/semantic-kernel/concepts/vector-store-connectors/vector-search?pivots=programming-language-csharp
-        //For now just update the first 10 players
         foreach (var playstyleDetail in playstyleDetails)
         {
-            // Create a record and generate a vector for the description using your chosen embedding generation implementation.
             await collection.UpsertAsync(await PlaystyleDetails.Create(playstyleDetail, textEmbeddingService));
         }
     }
